c6c137ce460c400a143a66aa8738d08bcc48875b
Chris Wanstrath <chris@ozmm.org>
2009-06-18 23:50:25 -0700

    remove old proof of concept

--- johnson.rb
+++
@@ -1,9 +0,0 @@
-# http://developer.apple.com/documentation/Cocoa/Reference/WebKit/ObjC_classic/index.html
-
-framework 'WebKit'
-
-webView = WebView.new
-object = webView.windowScriptObject
-
-object.evaluateWebScript("var name = 'chris'")
-puts object.evaluateWebScript("name")
