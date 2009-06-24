e3920d8de7bd32c038119c4c62d3f68204519930
Chris Wanstrath <chris@ozmm.org>
2009-06-23 22:57:49 -0700

    add ability to call arbitrary methods on ruby classes

--- lib/lyndon/ruby.rb
+++ lib/lyndon/ruby.rb
@@ -1,12 +1,17 @@
 module Lyndon
   class Ruby
-    def puts(*args)
-      super
-    end
-
+    ##
+    # Lets us call simple ruby methods
+    #
+    #   Ruby.IO_read(file)
+    #   Ruby.puts('hi')
     def invokeUndefinedMethodFromWebScript(name, withArguments:args)
       if respond_to? name
         send(name, *args)
+      elsif Kernel.respond_to? name
+        Kernel.send(name, *args)
+      elsif name =~ /^([A-Z][A-Za-z])_(\w+)/
+        Kernel.const_get($1).send($2, *args)
       end
     end
   end
