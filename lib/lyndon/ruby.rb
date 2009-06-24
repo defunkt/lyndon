48abdbbcfb9589fafe1366c4b265995b2ed61288
Chris Wanstrath <chris@ozmm.org>
2009-06-23 23:08:55 -0700

    call bang and predicate methods

--- lib/lyndon/ruby.rb
+++ lib/lyndon/ruby.rb
@@ -5,13 +5,23 @@ module Lyndon
     #
     #   Ruby.IO_read(file)
     #   Ruby.puts('hi')
+    #   Ruby.require('uri')
     def invokeUndefinedMethodFromWebScript(name, withArguments:args)
       if respond_to? name
         send(name, *args)
       elsif Kernel.respond_to? name
         Kernel.send(name, *args)
-      elsif name =~ /^([A-Z][A-Za-z])_(\w+)/
-        Kernel.const_get($1).send($2, *args)
+      elsif name =~ /^([A-Z][A-Za-z]+)_(.+)/
+        const = Kernel.const_get($1)
+        method = $2
+
+        if const.respond_to? method
+          const.send(method, *args)
+        elsif const.respond_to?("#{method}?")
+          const.send("#{method}?", *args)
+        elsif const.respond_to?("#{method}!")
+          const.send("#{method}!", *args)
+        end
       end
     end
   end
