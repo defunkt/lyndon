c850e7d3fd1b3c2edbd40d5433f897977100c340
Chris Wanstrath <chris@ozmm.org>
2009-06-25 22:58:25 -0700

    add eval_file for evalin' js with the Runtime, add lyndon.js and Lyndon.require()

--- lib/lyndon/runtime.rb
+++ lib/lyndon/runtime.rb
@@ -10,16 +10,26 @@ module Lyndon
       @scripter.setValue(Ruby.new, forKey:"Ruby")
 
       load_dom(dom) if dom
+      eval_file 'lib/js/lyndon'
+    end
+
+    def eval(js)
+      @scripter.evaluateWebScript(js)
+    end
+
+    def eval_file(file)
+      file = File.expand_path(file.to_s)
+      if File.exists? file
+        eval File.read(file)
+      elsif File.exists? file + '.js'
+        eval File.read(file + '.js')
+      end
     end
 
     def load_dom(dom, base_url = nil)
       dom = File.exists?(dom) ? File.read(dom) : dom
       @webView.mainFrame.loadHTMLString(dom, baseURL:base_url)
       NSApplication.sharedApplication.run
-    end
-
-    def eval(js)
-      @scripter.evaluateWebScript(js)
     end
   end
 end
